d5485778610502e0c4cb5b544fd52d8773eec273
au-mikey <64998409+au-mikey@users.noreply.github.com>
2020-12-02 09:22:34 +1030

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 sudo chmod +x initCam.sh
-sudo chmod +x watchdog.sh
+sudo chmod +x watchdog/watchdog.sh
 sudo cp systemFiles/* /etc/systemd/system/
 sudo systemctl enable initCam.service
-sudo systemctl enable rtspStream.service
+sudo systemctl enable PiCamWeb.service
+sudo systemctl enable PiCamWatchdog.service
 sudo systemctl start initCam.service
-sudo systemctl start rtspStream.service
-sudo echo */5 * * * * sudo /opt/rtspStream/watchdog.sh >> /etc/crontab
+sudo systemctl start PiCamWeb.service
+sudo systemctl start PiCamWatchdog.service
